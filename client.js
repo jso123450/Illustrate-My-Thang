f61585fd7d2dbd9da19b814603b5ff222b4998d9
kahsoonyap <kahsoonyap1998@hotmail.com>
2016-01-07 20:41:23 -0500

    Recieves and sends messages. Need to test with Javis's server

--- client.js
+++ client.js
@@ -2,10 +2,12 @@ $(document).ready(function(){
     var ws = io.connect("ws://104.131.91.167:5000");
 
     ws.on("serverMessage", function(msg){
-	$("chat").append("<p>message from server: " + msg + "</p>");
+	$("#chat").append("<p>message from server: " + msg + "</p>");
     });
 
-    function sendMessage(){
-	ws.emit("clientMessage", );
+    $("form#sendMessage").submit(function(e){
+	ws.emit("clientMessage", {msg: $("#chatBar").val()});
     }
+
+    
 });
