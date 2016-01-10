f4444af282236cb4a1576ff012e066d628283298
kahsoonyap <kahsoonyap1998@hotmail.com>
2016-01-10 17:16:51 -0500

    Tried to fix io error

--- static/client.js
+++ static/client.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     //var ws = io.connect("ws://104.131.91.167:5000");
+    
     var ws = io.connect("localhost:5000");
     ws.on("serverMessage", function(msg){
 	$("#chat").append("<p>message from server: " + msg + "</p>");
