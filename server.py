477dffce910179f0a020fb659dc41776641e70ca
Javis <wujavis@gmail.com>
2016-01-11 11:16:33 -0500

    OMG IT WORKS

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template 
 from flask.ext.socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-@app.route('/')
+@app.route('/', methods=["GET","POST"])
 def index():
     return render_template('chat.html')
 
