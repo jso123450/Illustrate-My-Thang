a97247c30bb31b383c32a770149f9432f4986b80
Kah Soon Yap <kahsoonyap1998@hotmail.com>
2016-01-11 11:43:46 -0500

    Magic has happened. Server is working btw. ~Javis : Can now send and recieve messages on chat. Will work to clean up chat and add connect/disconnect button

--- static/client.js
+++ static/client.js
@@ -2,14 +2,17 @@ $(document).ready(function(){
     //var ws = io.connect("ws://104.131.91.167:5000");
     
     var ws = io.connect("localhost:5000");
+
     ws.on("serverMessage", function(msg){
 	$("#chat").append("<p>message from server: " + msg + "</p>");
     });
 
-    $("form#sendMessage").submit(function(e){
-	ws.emit("clientMessage", {msg: $("#chatBar").val()});
-	console.log('good');
-    });
+    var sendMessage = function sendMessage(){
+	ws.emit("clientMessage", document.getElementById("chatBar").value);
+	document.getElementById("chatBar").value="";
+	//console.log('good');
+    }
 
-    
+    var sendMsg = document.getElementById("sendMsg");
+    sendMsg.addEventListener("click", sendMessage);
 });
