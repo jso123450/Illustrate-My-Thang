5afc6bba29bd28ace5b713d1b388a0d74e46b2be
Kah Soon Yap <kahsoonyap1998@hotmail.com>
2016-01-25 11:30:50 -0500

    background

--- static/illustrateMyThang.js
+++ static/illustrateMyThang.js
@@ -55,8 +55,9 @@ $(document).ready(function(){
     timerC.font="30px Impact";
     
     // Connects to the Server on port 5000
-    var ws  = io.connect("illustratemythang.chickenkiller.com:5000");
-    //var ws = io.connect("localhost:5000")
+    //var ws  = io.connect("illustratemythang.chickenkiller.com:5000");
+    var ws = io.connect("localhost:5000")
+    
     // The default userID (which will be changed)
     var userID = -1;
     
